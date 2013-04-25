600c949ab8706e58d1d00b136ed2426a4d8bd1b3
Felipe Riveroll Aguirre <friveroll@gmail.com>
2013-04-25 01:27:16 -0500

    Added avtar methods to model Profile

--- app/models/Profile.php
+++ app/models/Profile.php
@@ -17,4 +17,28 @@ class Profile extends Eloquent {
   {
     return $this->belongsTo('Pais', 'country_id');
   }
+
+  public function avtar_url()
+  {
+    if($filename = $this->avtar_file_name){
+      return URL::to($filename);
+    } else {
+      return URL::to('img/default-avtar.png');
+    }
+  }
+
+  public function avtar_path()
+  {
+    return path('public').'/assets/img/profiles/'.$filename;
+  }
+
+  public function delete_avatar()
+  {
+    if($this->avtar_file_name){
+      File::delete($this->avtar_path());
+      $this->avtar_file_name = null;
+      $this->save();
+      return true;
+    }
+  }
 }
