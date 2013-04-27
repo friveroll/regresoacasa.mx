3b18512fee9c6641e86bc4650709626fd8993c6c
Felipe Riveroll Aguirre <friveroll@gmail.com>
2013-04-27 00:36:37 -0500

    added config file for ide helper

---
+++ app/config/packages/barryvdh/laravel-ide-helper/config.php
@@ -0,0 +1,74 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filename
+    |--------------------------------------------------------------------------
+    |
+    | The default path to the helper file
+    |
+    */
+
+    'filename' => '_IDE_helper.php',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sublime version
+    |--------------------------------------------------------------------------
+    |
+    | Use a different code format, better for SublimeText (instead of Netbeans/phpStorm)
+    | Can also be used with the --sublime (-S) option.
+    |
+    */
+
+    'sublime' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Helper files to include
+    |--------------------------------------------------------------------------
+    |
+    | Include helper files. By default not included, but can be toggled with the
+    | -- helpers (-H) option. Extra helper files can be included.
+    |
+    */
+
+    'include_helpers' => false,
+
+    'helper_files' => array(
+        base_path().'/vendor/laravel/framework/src/Illuminate/Support/helpers.php',
+    ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Replaced classes (Managers)
+    |--------------------------------------------------------------------------
+    |
+    | These implementations cannot be found directly, because of a Manager class.
+    |
+    */
+
+    'replace' => array(
+        'Illuminate\Auth\AuthManager'           => 'Illuminate\Auth\Guard',
+        'Illuminate\Cache\CacheManager'         => 'Illuminate\Cache\StoreInterface',
+        'Illuminate\Database\DatabaseManager'   => 'Illuminate\Database\Connection',
+        'Illuminate\Queue\QueueManager'         => 'Illuminate\Queue\QueueInterface',
+        'Illuminate\Redis\RedisManager'         => 'Illuminate\Redis\Database',
+    ),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Only extend
+    |--------------------------------------------------------------------------
+    |
+    | These implementations aren't called static, so only extend them.
+    |
+    */
+
+    'only_extend' => array(
+        'Seeder',
+    ),
+
+);
